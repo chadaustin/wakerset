c403ead2d20c94a8a3968ef4bc2ed0f25e99c960
Chad Austin <chad@chadaustin.me>
2025-06-12 21:36:53 -0400

    bench: more realistic example

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,39 @@
-use divan::black_box;
 use divan::Bencher;
+use divan::black_box;
+use std::sync::atomic::Ordering;
 use std::pin::pin;
+use std::sync::Arc;
+use std::sync::atomic::AtomicU64;
+use std::task::Waker;
 use wakerset::WakerList;
+use wakerset::WakerSlot;
+
+struct Task {
+    wake_count: AtomicU64,
+}
+
+impl Task {
+    fn new() -> Arc<Self> {
+        Arc::new(Self {
+            wake_count: AtomicU64::new(0),
+        })
+    }
+
+    fn waker(self: &Arc<Self>) -> Waker {
+        self.clone().into()
+    }
+
+    #[allow(unused)]
+    fn wake_count(&self) -> u64 {
+        self.wake_count.load(Ordering::Acquire)
+    }
+}
+
+impl std::task::Wake for Task {
+    fn wake(self: Arc<Self>) {
+        self.wake_count.fetch_add(1, Ordering::AcqRel);
+    }
+}
 
 fn main() {
     divan::main()
@@ -9,7 +41,16 @@ fn main() {
 
 #[divan::bench]
 fn extract_some_wakers(bencher: Bencher) {
-    let mut wakers = WakerList::new();
+    let mut wakers = black_box(WakerList::new());
     let mut wakers = pin!(wakers);
-    bencher.bench_local(|| black_box(wakers.as_mut().extract_some_wakers()));
+    let slot = pin!(WakerSlot::new());
+    let task = Task::new();
+    wakers.as_mut().link(slot, task.waker());
+    let mut wakers = black_box(wakers);
+    bencher.bench_local(|| {
+        let mut w = wakers.as_mut().extract_some_wakers();
+        while w.wake_all() {
+            w.extract_more(wakers.as_mut());
+        }
+    });
 }
